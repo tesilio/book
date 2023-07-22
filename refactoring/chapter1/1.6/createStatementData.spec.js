42d28af00cb0da69e7d93b1cb1bb86cb5988d57e
tesilio <lodcoco@gmail.com>
2023-07-22 14:16:36 +0900

    refactor: 함수를 파일별로 분리

---
+++ refactoring/chapter1/1.6/createStatementData.spec.js
@@ -0,0 +1,98 @@
+import { expect } from 'chai';
+import createStatementData from './createStatementData.js';
+
+const sampleInvoiceData = () => {
+  return {
+    customer: 'BigCo',
+    performances: [
+      {
+        playID: 'hamlet',
+        audience: 55,
+      },
+      {
+        playID: 'as-like',
+        audience: 35,
+      },
+      {
+        playID: 'othello',
+        audience: 40,
+      },
+    ],
+  };
+};
+
+const samplePlaysData = () => {
+  return {
+    hamlet: {
+      name: 'Hamlet',
+      type: 'tragedy',
+    },
+    'as-like': {
+      name: 'As You Like It',
+      type: 'comedy',
+    },
+    othello: {
+      name: 'Othello',
+      type: 'tragedy',
+    },
+  };
+};
+
+const expectedResult = () => {
+  return {
+    'customer': 'BigCo',
+    'performances': [
+      {
+        'playID': 'hamlet',
+        'audience': 55,
+        'play': {
+          'name': 'Hamlet',
+          'type': 'tragedy',
+        },
+        'amount': 65000,
+        'volumeCredits': 25,
+      },
+      {
+        'playID': 'as-like',
+        'audience': 35,
+        'play': {
+          'name': 'As You Like It',
+          'type': 'comedy',
+        },
+        'amount': 58000,
+        'volumeCredits': 12,
+      },
+      {
+        'playID': 'othello',
+        'audience': 40,
+        'play': {
+          'name': 'Othello',
+          'type': 'tragedy',
+        },
+        'amount': 50000,
+        'volumeCredits': 10,
+      },
+    ],
+    'totalAmount': 173000,
+    'totalVolumeCredits': 47,
+  };
+}
+
+describe('1.6.js 테스트', () => {
+  describe('createStatementData', () => {
+    it('예상한 값과 일치한다', () => {
+      // Given
+      const invoice = sampleInvoiceData();
+      const plays = samplePlaysData();
+
+      // When
+      const result = createStatementData(invoice, plays);
+
+      // Then
+      expect(result).deep.equal(expectedResult());
+    });
+  });
+});
+
+
+
---
+++ refactoring/chapter1/1.6/statment.js
@@ -0,0 +1,27 @@
+import createStatementData from './createStatementData.js';
+
+export default function statement(invoice, plays) {
+  return renderPlainText(createStatementData(invoice, plays));
+  function renderPlainText(data) {
+    let result = `청구 내역 (고객명: ${data.customer})\n`;
+
+    for (let perf of data.performances) {
+      // 청구 내역 출력
+      result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience})석)\n`;
+    }
+
+    result += `총액: ${usd(data.totalAmount)}\n`;
+    result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+    return result;
+
+    function usd(aNumber) {
+      return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+      }).format(aNumber / 100);
+    }
+  }
+}
+
+
