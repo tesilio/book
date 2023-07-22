19de88ea87da359a6b032950bb82d08167d3f830
tesilio <lodcoco@gmail.com>
2023-07-22 14:57:18 +0900

    chore: 1.8 경로 추가

---
+++ refactoring/chapter1/1.8/createStatementData.js
@@ -0,0 +1,63 @@
+export default function createStatementData(invoice, plays) {
+  const result = {};
+    result.customer = invoice.customer;
+    result.performances = invoice.performances.map(enrichPerformance);
+    result.totalAmount = totalAmount(result);
+    result.totalVolumeCredits = totalVolumeCredits(result);
+    return result;
+
+  function enrichPerformance(aPerformance) {
+    const result = Object.assign({}, aPerformance); // 얕은 복사 수행
+    result.play = playFor(result); // 중간 데이터에 연극 정보를 저장
+    result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
+    return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  // 한 번의 공연에 대한 요금을 계산하는 함수 -> switch 문에서 추출함
+  function amountFor(aPerformance) { // 값이 바뀌지 않는 변수는 매개변수로 전달
+    let result = 0; // 명확한 이름으로 변경
+    switch (aPerformance.play.type) {
+      case 'tragedy': // 비극
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case 'comedy': // 희극
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
+    }
+    return result;
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    // 포인트를 적립한다.
+    result += Math.max(aPerformance.audience - 30, 0);
+
+    // 희극 관객 5명마다 추가 포인트를 제공한다.
+    if ('comedy' === aPerformance.play.type) {
+      result += Math.floor(aPerformance.audience / 5);
+    }
+    return result;
+  }
+
+  function totalAmount(data) {
+    return data.performances.reduce((total, p) => total + p.amount, 0);
+  }
+
+  function totalVolumeCredits(data) {
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0)
+  }
+}
