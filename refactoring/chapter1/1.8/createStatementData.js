5261c79f7d8e102e4636471b4e17da7707595d38
tesilio <lodcoco@gmail.com>
2023-07-24 22:24:33 +0900

    refactor: ComedyCalculator.volumeCredits 오버라이드

--- refactoring/chapter1/1.8/createStatementData.js
+++ refactoring/chapter1/1.8/createStatementData.js
@@ -52,15 +52,7 @@ class PerformanceCalculator {
   }
 
   get volumeCredits() {
-    let result = 0;
-    // 포인트를 적립한다.
-    result += Math.max(this.performance.audience - 30, 0);
-
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ('comedy' === this.play.type) {
-      result += Math.floor(this.performance.audience / 5);
-    }
-    return result;
+    return Math.max(this.performance.audience - 30, 0);
   }
 }
 
@@ -93,5 +85,9 @@ class ComedyCalculator extends PerformanceCalculator {
     }
     result += 300 * this.performance.audience;
     return result;
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
   }
 }
