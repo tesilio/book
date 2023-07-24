b8b65c20f2677808e0c746555dba144a71bb79ef
tesilio <lodcoco@gmail.com>
2023-07-24 21:29:05 +0900

    refactor: PerformanceCalculator.volumeCredits 클래스 메서드로 분리

--- refactoring/chapter1/1.8/createStatementData.js
+++ refactoring/chapter1/1.8/createStatementData.js
@@ -9,9 +9,9 @@ export default function createStatementData(invoice, plays) {
   function enrichPerformance(aPerformance) {
     const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance); // 얕은 복사 수행
-    result.play = playFor(result); // 중간 데이터에 연극 정보를 저장
-    result.amount = amountFor(result);
-    result.volumeCredits = volumeCreditsFor(result);
+    result.play = calculator.play;
+    result.amount = calculator.amount;
+    result.volumeCredits = calculator.volumeCredits;
     return result;
   }
 
@@ -25,15 +25,8 @@ export default function createStatementData(invoice, plays) {
   }
 
   function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    // 포인트를 적립한다.
-    result += Math.max(aPerformance.audience - 30, 0);
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).volumeCredits;
 
-    // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if ('comedy' === aPerformance.play.type) {
-      result += Math.floor(aPerformance.audience / 5);
-    }
-    return result;
   }
 
   function totalAmount(data) {
@@ -72,6 +65,18 @@ class PerformanceCalculator {
         break;
       default:
         throw new Error(`알 수 없는 장르: ${this.play.type}`);
+    }
+    return result;
+  }
+
+  get volumeCredits() {
+    let result = 0;
+    // 포인트를 적립한다.
+    result += Math.max(this.performance.audience - 30, 0);
+
+    // 희극 관객 5명마다 추가 포인트를 제공한다.
+    if ('comedy' === this.play.type) {
+      result += Math.floor(this.performance.audience / 5);
     }
     return result;
   }
