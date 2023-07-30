87f15691134aafbf46f49c56220d2aecc98f2aab
tesilio <lodcoco@gmail.com>
2023-07-30 13:21:55 +0900

    refactor: 6.1 세 번째 예제 추가

---
+++ refactoring/chapter6/6.1/3. 지역 변수의 값을 변경할 때.js
@@ -0,0 +1,36 @@
+const Clock = {
+  today: new Date(),
+};
+
+function printBanner() {
+  console.log('********************');
+  console.log('****** 고객채무 ******');
+  console.log('********************');
+}
+
+function printDetails(invoice, outstanding) {
+  console.log(`고객명: ${invoice.customer}`);
+  console.log(`채무액: ${outstanding}`);
+  console.log(`마감일: ${invoice.dueDate.toLocaleTimeString()}`);
+}
+
+function recordDueDate(invoice) {
+  // 마감일(dueDate)을 기록한다.
+  const today = Clock.today;
+  invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+}
+
+function printOwing(invoice) {
+  let outstanding = 0;
+
+  printBanner();
+
+  // 미해결 채무(outstanding)를 계산한다.
+  for (const o of invoice.orders) {
+    outstanding += o.amount;
+  }
+
+  recordDueDate(invoice);
+
+  printDetails(invoice, outstanding);
+}
