d4d8302005284b9d63fe1337fc9e89967c09d402
tesilio <lodcoco@gmail.com>
2023-07-31 23:20:58 +0900

    refactor: 6.3 첫 번째 예시 추가

---
+++ refactoring/6장/6.3 변수 추출하기/1. 간단한 계산식.js
@@ -0,0 +1,13 @@
+function price(order) {
+  // 가격(price) = 기본 가격 - 수량 할인 + 배송비
+  return order.quantity * order.itemPrice - Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 + Math.min(order.quantity * order.itemPrice * 0.1, 100);
+}
+
+/*****************/
+
+function price(order) {
+  const basePrice = order.quantity * order.itemPrice;
+  const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  const shipping = Math.min(basePrice * 0.1, 100);
+  return basePrice - quantityDiscount + shipping;
+}
