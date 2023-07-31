67ef33ffe3632cde8626dc1a16fea3e2ebf9f2a9
tesilio <lodcoco@gmail.com>
2023-07-31 22:50:45 +0900

    refactor: 6.2 세 번째 예시 인라인화

---
+++ refactoring/chapter6/6.2/3. 세 번째 예시.js
@@ -0,0 +1,21 @@
+function reportLines(aCustomer) {
+  const lines = [];
+  gatherCustomerData(lines, aCustomer);
+  return lines;
+}
+
+function gatherCustomerData(out, aCustomer) {
+  out.push(['name', aCustomer.name]);
+  out.push(['location', aCustomer.location]);
+}
+
+
+/********************/
+
+
+function reportLines(aCustomer) {
+  const lines = [];
+  lines.push(['name', aCustomer.name]);
+  lines.push(['location', aCustomer.location]);
+  return lines;
+}
