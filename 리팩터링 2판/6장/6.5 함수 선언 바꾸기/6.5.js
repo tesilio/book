0d4080506b119356af651794ab987ba55d4b3daa
tesilio <lodcoco@gmail.com>
2023-08-07 22:53:19 +0900

    refactor: 6.5절 추가

--- 리팩터링 2판/6장/6.5 함수 선언 바꾸기/6.5.js
+++ 리팩터링 2판/6장/6.5 함수 선언 바꾸기/6.5.js
@@ -31,7 +31,92 @@ function circum(radius) {
 function circumference(radius) {
   return 2 * Math.PI * radius;
 }
-
 // 3. 수정한 코드를 테스트
-
 // 4. 예전 함수를 인라인(6.2절)
+// 5. 모두 바꿨다면 기존 함수 모두 삭제
+
+/**
+ * 매개변수 추가하기
+ */
+class Book {
+  _reservations;
+  addReservation(customer) {
+    this._reservations.push(customer);
+  }
+}
+
+// 2. 본문을 새로운 함수로 추출
+class Book {
+  _reservations;
+  addReservation(customer) {
+    this.zz_addReservation(customer);
+  }
+
+  zz_addReservation(customer) {
+    this._reservations.push(customer)
+  }
+}
+
+// 3. 새 함수의 선언문과 호출문에 원하는 매개변수를 추가
+class Book {
+  _reservations;
+  addReservation(customer) {
+    this.zz_addReservation(customer, false);
+  }
+
+  zz_addReservation(customer, isPriority) {
+    assert(isPriority === true || isPriority ===false);
+    this._reservations.push(customer)
+  }
+}
+
+
+
+/**
+ * 매개변수를 속성으로 바꾸기
+ */
+function inNewEngland(aCustomer) {
+  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(aCustomer.address.state);
+}
+
+const newEnglanders = someCustomers.filter(c => inNewEngland(c));
+
+// 1. 매개변수로 사용할 코드를 변수로 추출
+function inNewEngland(aCustomer) {
+  const stateCode = aCustomer.address.state;
+  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(stateCode);
+}
+
+// 2. 함수 추출하기
+function xxNewinNewEngland(stateCode) {
+  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(stateCode);
+}
+function inNewEngland(aCustomer) {
+  const stateCode = aCustomer.address.state;
+  return xxNewinNewEngland(stateCode);
+}
+
+// 3. 기존 함수의 입력 매개변수 인라인하기
+function xxNewinNewEngland(stateCode) {
+  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(stateCode);
+}
+function inNewEngland(aCustomer) {
+  return xxNewinNewEngland(aCustomer.address.state);
+}
+
+// 4. 함수 인라인하기로 기존 함수의 본문을 호출문에 집어넣기
+function xxNewinNewEngland(stateCode) {
+  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(stateCode);
+}
+function inNewEngland(aCustomer) {
+  return xxNewinNewEngland(aCustomer.address.state);
+}
+
+const newEnglanders = someCustomers.filter(c => xxNewinNewEngland(c.address.state));
+
+// 5. 함수 선언 바꾸기를 다시 적용하여 새 함수와 기존 함수의 이름 교체
+function inNewEngland(stateCode) {
+  return ['MA', 'CT', 'ME', 'VT', 'NH', 'RI'].includes(stateCode);
+}
+
+const newEnglanders = someCustomers.filter(c => inNewEngland(c.address.state));
