5643885cf7deb213648aa93d358b674317e0b964
tesilio <lodcoco@gmail.com>
2023-09-07 00:10:23 +0900

    refactor: 7.1절 두 번째 예제 완성

--- 리팩터링 2판/6장/7.1 레코드 캡슐화하기/7.1-2.js
+++ 리팩터링 2판/6장/7.1 레코드 캡슐화하기/7.1-2.js
@@ -1,7 +1,7 @@
 /**
  * 두 번째 예시
  */
-const customerData = {
+let customerData = {
   '1920': {
     name: '마틴 파울러',
     id: '1920',
@@ -51,4 +51,63 @@ function compareUsage(customerID, laterYear, month) {
   };
 }
 
-// todo: 241 -> 이번 캡슐화도 앞에서와 마찬가지로 변수 캡슐화66절부터 시작한다.
+// info: 변수 캡슐화
+function getRawDataOfCustomers() {
+  // return customerData;
+  // return customerData._data;
+  return customerData.rawData;
+}
+
+function setRawDataOfCustomers(arg) {
+  // customerData = arg;
+  customerData = new CustomerData(arg);
+}
+
+// info: 쓰기 예
+// getRawDataOfCustomers()[customerID].usages[year][month] = amount;
+// setUsage(customerID, year, month, amount);
+getCustomerData().setUsage(customerID, year, month, amount);
+// function setUsage(customerID, year, month, amount) {
+//   getRawDataOfCustomers()[customerID].usages[year][month] = amount;
+// }
+
+// info: 읽기 예
+function compareUsage(customerID, laterYear, month) {
+  // const later = getRawDataOfCustomers()[customerID].usages[laterYear][month];
+  // const earlier = getRawDataOfCustomers()[customerID].usages[laterYear - 1][month];
+
+  // info: 읽는 코드를 독립 함수로 추출하여 클래스 메서드로 옮겨서 호출
+  // const later = getCustomerData().usage(customerID, laterYear, month);
+  // const earlier = getCustomerData().usage(customerID, laterYear - 1, month);
+
+  // info: 복제한 값을 호출 - 단점 데이터 구조가 클수록 복제 비용이 커짐
+  const later = getCustomerData().rawData[customerID].usages[laterYear][month];
+  const earlier = getCustomerData().rawData[customerID].usages[laterYear - 1][month];
+  return {
+    laterAmount: later,
+    change: later - earlier,
+  }
+}
+
+// info: 전체 데이터 구조를 표현하는 클래스 정의
+class CustomerData {
+  constructor(data) {
+    this._data = data;
+  }
+
+  setUsage(customerID, year, month, amount) {
+    this._data[customerID].usages[year][month] = amount;
+  }
+
+  get rawData() {
+    return _.cloneDeep(this._data);
+  }
+
+  usage(customerID, year, month) {
+    return this._data[customerID].usages[year][month];
+  }
+}
+
+function getCustomerData() {
+  return customerData;
+}
