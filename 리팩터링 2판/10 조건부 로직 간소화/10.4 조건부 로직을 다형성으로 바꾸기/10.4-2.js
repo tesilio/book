f98509982fb222562d8299143173da6fb55fc86e
tesilio <lodcoco@gmail.com>
2023-10-15 14:03:07 +0900

    refactor: 10.4-2 voyageAndHistoryLengthFactor 서브클래스로 분리

--- 리팩터링 2판/10 조건부 로직 간소화/10.4 조건부 로직을 다형성으로 바꾸기/10.4-2.js
+++ 리팩터링 2판/10 조건부 로직 간소화/10.4 조건부 로직을 다형성으로 바꾸기/10.4-2.js
@@ -69,34 +69,14 @@ class Rating {
 
   get voyageAndHistoryLengthFactor() {
     let result = 0;
-    if (this.voyage.zone === '중국' && this.hasChinaHistory) {
-      result += 3;
+    if (this.history.length > 8) {
+      result += 1;
+    }
 
-      if (this.history.length > 10) {
-        result += 1;
-      }
-
-      if (this.voyage.length > 12) {
-        result += 1;
-      }
-
-      if (this.voyage.length > 18) {
-        result -= 1;
-      }
-    } else {
-      if (this.history.length > 8) {
-        result += 1;
-      }
-
-      if (this.voyage.length > 14) {
-        result -= 1;
-      }
+    if (this.voyage.length > 14) {
+      result -= 1;
     }
     return result;
-  }
-
-  get hasChinaHistory() {
-    return this.history.some(v => '중국' === v.zone);
   }
 }
 
@@ -105,6 +85,24 @@ class ExperiencedChinaRating extends Rating {
   get captainHistoryRisk() {
     const result = super.captainHistoryRisk - 2;
     return Math.max(result, 0);
+  }
+
+  get voyageAndHistoryLengthFactor() {
+    let result = 0;
+    result += 3;
+
+    if (this.history.length > 10) {
+      result += 1;
+    }
+
+    if (this.voyage.length > 12) {
+      result += 1;
+    }
+
+    if (this.voyage.length > 18) {
+      result -= 1;
+    }
+    return result;
   }
 }
 
